dd1772a55c5673a7f81f48dab69b23bca9da3c88
Fious <tltonylei@gmail.com>
2015-12-02 11:03:58 -0800

    add RangeDataModel

---
+++ server/XLSnipServer/XLSnipServer/Models/RangeDataModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XLSnipServer.Models
+{
+    public class RangeDataModel
+    {
+        public String Address { get; set; }
+        public String[][] Data { get; set; }
+    }
+}
