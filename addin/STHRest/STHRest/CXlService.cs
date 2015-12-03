92473d4ba0a6666664dca8f37cd4457cb9220b6d
hajime1 <hajime.kobayashi@ze.com>
2015-12-03 09:54:11 -0800

    Updated xl (added read/write file)

--- addin/STHRest/STHRest/CXlService.cs
+++ addin/STHRest/STHRest/CXlService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Runtime.InteropServices;
 using STHRest.Entites;
 using STHRest.Models;
+using System.IO;
 
  
 namespace STHRest
@@ -28,23 +29,63 @@ namespace STHRest
         public string UploadRange(string user, string name, string desc, string xlAddress, string fileName)
         {
             RangeDataEntity entity = new RangeDataEntity();
-
-            var data2 = new string[2][];
-            data2[0] = new String[] { "1", "2" };
-            data2[1] = new String[] { "3", "4" };
-
             string result;
-            
-            result = entity.UploadRange("Guest", "TestRange", "foobar", "$A1:$B2", data2);
+            string[][] data;
+            int rows;
+            int cols;
+
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs, Encoding.UTF8))
+                {
+                    rows = int.Parse(sr.ReadLine());
+                    cols = int.Parse(sr.ReadLine());
+                    sr.ReadLine();
+
+                    data = new string[rows][];
+                    for (int r = 0; r < rows; r++)
+                    {
+                        data[r] = new string[cols];
+                        for (int c = 0; c < cols; c++)
+                        {
+                            data[r][c] = sr.ReadLine();
+                        }
+                    }
+                }
+            }
+
+            result = entity.UploadRange(user, name, desc, xlAddress, data);
             return result;
         }
 
-        public RangeDataModel DownloadRange(int id)
+        public string DownloadRange(int id)
         {
+            string fileName = Path.GetTempPath() + "STH" + DateTime.Now.ToString("yyyymmdd hhnnss") + ".txt";
+
             RangeDataEntity entity = new RangeDataEntity();
             RangeDataModel result = entity.DownLoadRange(id);
 
-            return result;
+            using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    sw.WriteLine(result.Data.Length.ToString());
+                    sw.WriteLine(result.Data[0].Length.ToString());
+                    sw.WriteLine();
+
+                    for (int r = 0; r < result.Data.Length; r++)
+                    {
+                        for (int c = 0; c < result.Data[r].Length; c++)
+                        {
+                            sw.WriteLine(result.Data[r][c]);
+                        }
+                    }
+
+                    sw.Close();
+                }
+            }
+
+            return fileName;
         }
     }
 
