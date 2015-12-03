fc4ddb6f401a93deaea5893f2b1ea37a5f75aa8c
Fious <tltonylei@gmail.com>
2015-12-03 11:31:05 -0800

    add model field for UserName

--- addin/STHRest/STHRest/Models/RangeModel.cs
+++ addin/STHRest/STHRest/Models/RangeModel.cs
@@ -12,5 +12,6 @@ namespace STHRest.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Desc { get; set; }
+		public string UserName { get; set; } 
     }
 }
